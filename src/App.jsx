3a5391c9d054499a789ad0d3f522b553aaa6af9c
rubi-dahal <rrubidahal7@gmail.com>
2025-01-23 15:31:48 +0545

    add

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Quiz from './Components/Quiz/Quiz';
+import './Components/Quiz/Quiz.css';
+
+const App = () => {
+  return (
+    <div>
+      <Quiz />
+    </div>
+  )
+}
+
+export default App
